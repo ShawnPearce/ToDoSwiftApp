088accf1a7ae21784c5ad6e341fabf3ae7fec3a0
ShawnPearce <pearcsha@sheridancollege.ca>
2025-07-26 19:13:16 -0400

    Initial Commit

---
+++ A4ShawnPearce/A4ShawnPearceApp.swift
@@ -0,0 +1,17 @@
+//
+//  A4ShawnPearceApp.swift
+//  A4ShawnPearce
+//
+//  Created by Shawn Pearce on 2025-07-26.
+//
+
+import SwiftUI
+
+@main
+struct A4ShawnPearceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A4ShawnPearce/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A4ShawnPearce
+//
+//  Created by Shawn Pearce on 2025-07-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
